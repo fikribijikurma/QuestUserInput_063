542624164122046dd3597cc24d3c7bd8c3ce90f3
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-10-30 12:02:40 +0700

    membuat value textAlamat pada OutlinedTextField pada Row column

--- app/src/main/java/com/example/myquestuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/myquestuserinput/TeksEdit.kt
@@ -55,6 +55,16 @@ fun FormDataDiri(modifier: Modifier
             }
 
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(widht = 250.dp),
+            label = { Text(text = "Alamat Lengkap") },
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+
 
     }
 }
