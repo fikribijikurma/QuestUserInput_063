d2d7aa1064e100ebb9273a03b6b67ab24b6bd696
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-10-30 11:42:12 +0700

    menambahkan variabel-variabel pada FormDataDiri

--- app/src/main/java/com/example/myquestuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/myquestuserinput/TeksEdit.kt
@@ -2,7 +2,17 @@ package com.example.myquestuserinput
 
 import java.lang.reflect.Modifier
 
+@Composable
 fun FormDataDiri(modifier: Modifier
 ){
+    //variabel-variabel untuk mengingat nilai masukan dari keybord
+    var textNama by remember { mutableStateOf(value = "")}
+    var textAlamat by remember { mutableStateOf(value = "")}
+    var textJK by remember { mutableStateOf(value = "")}
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf(value = "")}
+    var alamat by remember { mutableStateOf(value = "")}
+    var jk by remember { mutableStateOf(value = "")}
 
 }
